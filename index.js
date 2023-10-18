007b86ebb012ea9d5d33b45b3d7abca8c79406a5
rayhan <rayhanmahmud990099@gmail.com>
2023-10-18 19:47:19 +0600

    complet the mongodb

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
+require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
-
-
-// DYQtmD4HujvIHnfI
-//brandShop
-
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lnrotgp.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,21 +28,31 @@ async function run() {
         await client.connect();
 
 
-        const brandCollection = client.db('branDB').collection('brand');
+        const brandCollection = client.db('brandDB').collection('brand');
+        // const reviewCollection = client.db('brandDB').collection('reviews');
+        const productCollection = client.db('brandDB').collection('products');
 
 
 
-        app.get('/brand', async (res, req) => {
+        app.get('/brand', async (req, res) => {
             const result = await brandCollection.find().toArray();
             res.send(result)
         })
 
-
-        app.post('/brand', async (res, req) => {
-            const data = req.body;
-            const result = await brandCollection.insertOne(data);
-            res.send(result);
+        app.get("/reviews", async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result)
         })
+
+
+
+        app.post("/products", async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            console.log(result);
+            res.send(result);
+        });
+
 
 
 
